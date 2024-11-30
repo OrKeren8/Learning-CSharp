33635893bbdad44560a699c8358ce24d9eef9f47
or keren <orkeren8@gmail.com>
2024-11-30 16:23:23 +0200

    validate binary number

--- EX01 Or 315155531 Roy 315254243/Ex01_01/Program.cs
+++ EX01 Or 315155531 Roy 315254243/Ex01_01/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 
 namespace Ex01_01
 {
-    internal class Program
+    class Program
     {
+        private const int k_BinaryNumberLength = 8;
+        
+        static void main()
+        {
+
+        }
+
+        private bool validateBinaryNumber(string i_binaryNumber)
+        {
+            bool isValidBinaryNumber = true;
+            bool isBinaryDigit = true;
+
+            if (i_binaryNumber.Length != k_BinaryNumberLength)
+            {
+                isValidBinaryNumber = false;
+            }
+            if (isValidBinaryNumber)
+            {
+                for (int i=0; i < k_BinaryNumberLength && isBinaryDigit; i++)
+                {
+                    isBinaryDigit = checkBinaryDigit(i_binaryNumber[i]); 
+                }             
+            }
+            return (isValidBinaryNumber && isBinaryDigit);
+        }
+
+        private bool checkBinaryDigit(char i_binaryDigit)
+        {
+            return (i_binaryDigit == '0' || i_binaryDigit == '1');
+        }
     }
 }
