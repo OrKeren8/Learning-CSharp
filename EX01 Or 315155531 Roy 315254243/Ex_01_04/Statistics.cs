e2e98bbe10c5a4087666e6b4e9b3b1576196c0ff
or keren <orkeren8@gmail.com>
2024-12-03 21:13:47 +0200

    finish the palindrom checking

---
+++ EX01 Or 315155531 Roy 315254243/Ex_01_04/Statistics.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Ex_01_04
+{
+    internal class Statistics
+    {
+        public static bool CheckIfPalindrom(string i_string)
+        {
+
+            if (i_string.Length < 2)
+            {
+                return true;
+            }
+            else
+            {
+                if (i_string[0] == i_string[i_string.Length - 1])
+                {
+                    return CheckIfPalindrom(i_string.Substring(1, i_string.Length - 2));
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ EX01 Or 315155531 Roy 315254243/Ex_01_04/UserInterface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ex_01_04
+{
+    internal class UserInterface
+    {
+        protected const int k_valid_string_name = 10;
+        
+        public static void StringStatisticsChecker()
+        {
+            string userInputString;
+            getValidString(out userInputString);
+            Console.WriteLine();
+            isPalindrom(userInputString);
+
+        } 
+        private static void getValidString(out string o_userInputString)
+        {
+            bool isValidString = false;
+            string tmpString = "";
+            while (isValidString == false)
+            {
+                Console.WriteLine("Please enter 10 charecters string");
+                tmpString = Console.ReadLine();
+
+                isValidString = validateString(tmpString);
+            }
+            o_userInputString = tmpString;
+        }
+
+        private static bool validateString(string i_userInputString) {
+            bool isValidString = true;
+            if (i_userInputString.Length != 10)
+            {
+                isValidString = false;
+            }
+
+            return isValidString;
+        }
+        private static void isPalindrom(string i_userInputString)
+        {
+            bool isPalindrom = false;
+            isPalindrom = Statistics.CheckIfPalindrom(i_userInputString);
+            if(isPalindrom)
+            {
+                Console.WriteLine("The String Is Palindrom");
+            }
+            else
+            {
+                Console.WriteLine("The String Isn't Palindrom");
+            }
+        }
+        
+    }
+}
