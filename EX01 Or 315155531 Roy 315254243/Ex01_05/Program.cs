dc10f831236f075c3b946ec7026e32723d2d4836
or keren <orkeren8@gmail.com>
2024-12-04 23:16:15 +0200

    exercise 5 works!

---
+++ EX01 Or 315155531 Roy 315254243/Ex01_05/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace Ex01_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                UserInterface.PrintStatistics();
+            }
+        }
+    }
+}
---
+++ EX01 Or 315155531 Roy 315254243/Utils/Validator.cs
@@ -0,0 +1,20 @@
+﻿namespace Utils
+{
+    internal class Validator
+    {
+        public static bool IsLowerDigit(char i_digit)
+        {
+            return (i_digit >= 'a' && i_digit <= 'z');
+        }
+
+        public static bool IsDigitNumber(char i_digit)
+        {
+            return (i_digit >= '0' && i_digit <= '9');
+        }
+
+        public static bool IsUpperDigit(char i_digit)
+        {
+            return (i_digit >= 'A' && i_digit <= 'Z');
+        }
+    }
+}
