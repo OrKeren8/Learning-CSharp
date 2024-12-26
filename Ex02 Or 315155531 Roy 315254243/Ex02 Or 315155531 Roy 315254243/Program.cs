3d5ee98451f45d5651ea2332660481278957ff82
or keren <orkeren8@gmail.com>
2024-12-26 09:19:03 +0200

    add player class

---
+++ Ex02 Or 315155531 Roy 315254243/Ex02 Or 315155531 Roy 315254243/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex02_Or_315155531_Roy_315254243
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
---
+++ Ex02 Or 315155531 Roy 315254243/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace Player
+{
+    public class Player
+    {
+        private string m_Name;
+        private int m_ID;
+
+        public string Name
+        {
+            get { return m_Name; }
+            private set { m_Name = value; }
+        }
+
+        public int ID
+        {
+            get { return m_ID; }
+            private set { m_ID = value; }
+        }
+
+        //constructor
+        public Player(string i_Name, int i_ID)
+        {
+            Name = i_Name;
+            ID = i_ID;
+        }
+
+        public void Play()
+        {
+
+        }
+    }
+}
