b400016e5ec3ceac478797dc2c1d8e41aa3fd345
or keren <orkeren8@gmail.com>
2025-01-21 23:57:03 +0200

    create first structure for events

---
+++ Ex04 OrKeren 315155531 RoyKalfon 315254243/Ex04.Menus.Events/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex04.Menus.Events
+{
+    public class Item
+    {
+        public string Header { get; private set; }
+        internal List<Item> Items { get; set; } = new List<Item> { };
+        public Menu Prev { get; private set; } = null;
+
+        public event Action m_OnActionDelegates;
+
+        public Item(string i_Header, Menu prev)
+        {
+            this.Header = i_Header;
+            Prev = prev;
+            prev?.AddItem(this);
+        }
+
+        public void AddItem(Item i_Item)
+        {
+            this.Items.Add(i_Item);
+        }
+
+        internal void notifyObservers()
+        {
+            if (this.m_OnActionDelegates != null)
+            {
+                this.m_OnActionDelegates.Invoke();
+            }
+        }
+
+        public virtual void Show() { }
+    }
+}
---
+++ Ex04 OrKeren 315155531 RoyKalfon 315254243/Ex04.Menus.Events/Menu.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Ex04.Menus.Events
+{
+    public class Menu : Item
+    {
+
+        private readonly List<string> ZeroString = new List<string> { "exit", "go back" };
+
+        public Menu(string i_Header, Menu i_Prev) : base(i_Header, i_Prev){}
+
+        public override void Show()
+        {
+            printHeader();
+            for (int i = 1; i < this.Items.Count; i++)
+            {
+                this.printItem(this.Items[i], i);
+            }
+
+            int zeroStringIndex = (this.Prev != null) ? 1 : 0;
+            Console.WriteLine($"0. to {this.ZeroString[zeroStringIndex]}");
+
+            int userChoice = this.getValidChoiceFromUser();
+            if(userChoice == 0)
+            {
+                if(this.Prev != null)
+                {
+                    this.goBack();
+                }
+            }
+            else
+            {
+                Items[userChoice].notifyObservers();
+            }
+        }
+
+        private void printItem(Item i_Item, int i_Index)
+        {
+            Console.WriteLine($"{i_Index}. {i_Item.Header}");
+        }
+
+        private void printHeader()
+        {
+            Console.WriteLine($"** {this.Header} **");
+            Console.WriteLine("------------------------");
+        }
+
+        private int getValidChoiceFromUser()
+        {
+            string userChoice;
+            int itemIndex = 0;
+            int max = this.Items.Count;
+            bool isValid = false, parseSuccess = false;
+
+            while (!(isValid && parseSuccess))
+            {
+                int zeroStringIndex = (this.Prev != null) ? 1 : 0;
+                Console.WriteLine($"Please enter your choice (1-{max} or 0 to {this.ZeroString[zeroStringIndex]})");
+                userChoice = Console.ReadLine();
+                parseSuccess = int.TryParse(userChoice, out itemIndex);
+                if (parseSuccess && itemIndex <= max)
+                {
+                    isValid = true;
+                }
+            }
+
+            return itemIndex;
+        }
+
+        private void goBack()
+        {
+            Console.Clear();
+            this.Prev.Show();
+        }
+    }
+}
